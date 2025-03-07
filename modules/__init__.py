e25f9db291aaa0469910297862b8efcb140b1b84
thaygiaotoankute <158003018+thaygiaotoankute@users.noreply.github.com>
2025-03-07 13:03:55 +0700

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,15 @@
+# This file makes the modules directory a Python package
+# It allows importing the modules as "from modules.xxx import yyy"
+
+__all__ = [
+    'bbt_web',
+    'bbtv2_web',
+    'graph_web',
+    'dothi_web',
+    'mathpix_web',
+    'addtrue_web',
+    'hkg_web',
+    'def_cal',
+    'def_bbt',
+    'def_hkg'
+]
